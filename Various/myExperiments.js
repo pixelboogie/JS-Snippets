367f73fb151a1296178e8653cebba4a2818823dc
Shane Anderson <shane@pixelboogie.com>
2019-11-21 15:52:14 -0800

    No biggie

---
+++ Various/myExperiments.js
@@ -0,0 +1,14 @@
+function addThem(num1, num2){
+    console.log("Answer: " + (num1 + num2));
+}
+
+addThem(1,2);
+
+var addThem2 = (num1, num2) => console.log("Answer: " + (num1 + num2));
+
+addThem2(1,2);
+
+
+
+
+
---
+++ Various/myReduce.js
@@ -0,0 +1,9 @@
+let arr = [1,2];
+
+let sum = arr.reduce((acc, val) => {
+    return acc + val;
+  });
+
+  console.log(sum);
+
+
