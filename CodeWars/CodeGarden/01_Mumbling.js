628b3fb0b9146d7e34daba67dd2bae51c2135280
Shane Anderson <shane@pixelboogie.com>
2019-11-26 11:51:31 -0800

    ok

--- CodeWars/CodeGarden/01_Mumbling.js
+++ CodeWars/CodeGarden/01_Mumbling.js
@@ -1,6 +1,5 @@
-https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
-
-
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+// https://www.codewars.com/kata/mumbling/train/javascript
 // This time no story, no theory. The examples below show you how to write function accum:
 
 // Examples:
@@ -14,25 +13,23 @@ function accum(s) {
     // a place to store the result
     let result = "";
     //iterate over sting
-    for(let i=0; i>s.length; i++){
-    // append the current letter i+1 times to the string
-        for(let j=0; j<i+1; j++>){
-            if(j==0){
+    for (let i = 0; i < s.length; i++) {
+        // append the current letter i+1 times to the string
+        for (let j = 0; j < i + 1; j++) {
+            if (j == 0) {
                 // ap the first append
                 result += s[i].toUpperCase();
+            } else {
+                result += s[i].toLowerCase();
             }
-     
         }
-    
- 
-    // append a dash if it is not the last letter
-
+            // append a dash if it is not the last letter
+            if (i != s.length-1) {
+                result += '-';
+            }
     }
-
-
-
-
+        return result;
 }
 
-
+console.log(accum("cwAt"));
 
